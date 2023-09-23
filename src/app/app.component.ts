c9af5032f2583ff3109aad2b75756d2f04d4e621
foykon <foykon9@gmail.com>
2023-09-23 14:24:18 +0300

    notification service for ui with toastr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessagePosition, ToastrMessageType } from './services/ui/custom-toastr.service';
 declare var $: any
 
 @Component({
@@ -8,8 +10,16 @@ declare var $: any
 })
 export class AppComponent {
   title = 'ETicaretClient';
-}
+  /**
+   *
+   */
+  constructor(private toastrService: CustomToastrService) {
+    toastrService.message("merhaba", "furkan", {
+      messageType: ToastrMessageType.Info,
+      messagePosition: ToastrMessagePosition.BottomFullWidth
+    });
 
-$(document).ready(()=>{
-  //alert("asd")
-})
+
+  }
+
+}
